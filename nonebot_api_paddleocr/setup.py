3de2c90f4794c5d6ed1b1ab7a832183bd1f21dac
canxin121 <69547456+canxin121@users.noreply.github.com>
2023-04-04 21:26:59 +0800

    paddle

--- nonebot_api_paddleocr/setup.py
+++ nonebot_api_paddleocr/setup.py
@@ -5,7 +5,7 @@ with open("readme.rst", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="nonebot_api_paddle",  # 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
-    version="0.1.6",  # 程序版本
+    version="0.1.7",  # 程序版本
     author="canxin",  # 项目作者
     author_email="1969730106@qq.com",  # 作者邮件
     description="nonbot_api_paddle",  # 项目的一句话描述
@@ -21,8 +21,8 @@ setuptools.setup(
     ],
     install_requires=[
         'nonebot2>=2.0.0rc3',
-        'nonebot-adapter-onebot>=2.0.0b1'
-        'requests>=2.28.2'
+        'nonebot-adapter-onebot>=2.0.0b1',
+        'requests>=2.28.2',
         'ffmpeg>=1.4'
     ]
 )
